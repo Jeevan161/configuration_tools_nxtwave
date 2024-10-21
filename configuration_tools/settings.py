65fd1cb5a5ad43ec6bb8c1e8eba4939eb13bf932
Jeevan161 <111897392+Jeevan161@users.noreply.github.com>
2024-10-21 21:46:18 +0530

    Update settings.py

--- configuration_tools/settings.py
+++ configuration_tools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c9j^9xytgsh20x*y0n0_kh!ky@p!9ebj(06(q8teo_6senio0m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
