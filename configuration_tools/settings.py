f790d9d786b49e566a8a9eeaee822878e45a0240
Jeevan161 <111897392+Jeevan161@users.noreply.github.com>
2024-10-22 23:11:40 +0530

    Update settings.py

--- configuration_tools/settings.py
+++ configuration_tools/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Define a directory for static files collection
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
